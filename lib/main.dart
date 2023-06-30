5e02cc829506636d4763173e9edb1b4e00366d7e
aldy202 <aldyfauzi202@.gmail.com>
2023-07-01 03:35:52 +0700

    mulai membuat halaman home

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'NatureHike',
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Home(),
     );
   }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,11 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var size = MediaQuery.of(context)
+      .size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 112, 255, 167),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/gelombang.png"),
+                
+              )
+            ),
+          ),
+          SafeArea(
+            child: Column(
+            children: <Widget>[
+              Container(
+                height: 52,
+                width: 52,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 87, 255, 162),
+                  shape: BoxShape.circle,
+                ),
+              )
+            ]),
+          )
+
+          
+        ]),
+    );
   }
 }
